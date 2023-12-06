418ccde5b24043fad5a11a0efa483b71fbfb597f
Dev <hoangtv1809@gmail.com>
2023-12-06 17:16:27 +0700

    Update Hoàn Chỉnh

--- dao/typefood.php
+++ dao/typefood.php
@@ -50,23 +50,3 @@ function getone_type_food($ID){
     $sql = "SELECT * FROM type_food WHERE ID =".$ID;
     return pdo_query_one($sql);
 }
-// /**
-//  * Truy vấn một loại theo mã
-//  * @param int $ma_loai là mã loại cần truy vấn
-//  * @return array mảng chứa thông tin của một loại
-//  * @throws PDOException lỗi truy vấn
-//  */
-// function loai_select_by_id($ma_loai){
-//     $sql = "SELECT * FROM loai WHERE ma_loai=?";
-//     return pdo_query_one($sql, $ma_loai);
-// }
-// /**
-//  * Kiểm tra sự tồn tại của một loại
-//  * @param int $ma_loai là mã loại cần kiểm tra
-//  * @return boolean có tồn tại hay không
-//  * @throws PDOException lỗi truy vấn
-//  */
-// function loai_exist($ma_loai){
-//     $sql = "SELECT count(*) FROM loai WHERE ma_loai=?";
-//     return pdo_query_value($sql, $ma_loai) > 0;
-// }
